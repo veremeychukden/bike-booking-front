1938bd562a0342147adfb529e175b654848554c2
veremeychukden <veremeychukden@gmail.com>
2023-12-26 21:33:58 +0200

    Create header components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Header from './header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+  return(
+    <div className="header">
+      <h1 className="title">ADMIN.BIKE-BOOKING.COM</h1>
+    </div>
+  )
+}
+
+export default Header;
